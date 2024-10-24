8305d89ed52c028e3b275cf1643e2cb665d24760
mohitejaikumar <mohitjaikumar@gmail.com>
2024-10-24 17:33:25 +0530

    vercel.json src-dest added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,17 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import CloudIDE from './components/CloudIDE'
 import LandingPage from './components/LandingPage'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <>
+      <BrowserRouter>
       <Routes>
         <Route path="/ide/:id" element={<CloudIDE />} />
         <Route path="/" element={<LandingPage />} />
       </Routes>
+      </BrowserRouter>
     </>
   )
 }
