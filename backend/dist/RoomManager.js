0cd87bdbe1882dea952be51ceb415257d3b6380e
mohitejaikumar <mohitejaikumar@gmail.com>
2024-12-11 14:24:53 +0530

    removed logic to stop container

--- backend/dist/RoomManager.js
+++ backend/dist/RoomManager.js
@@ -1,117 +1,151 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
-const node_pty_1 = require("node-pty");
+const node_pty_1 = __importDefault(require("node-pty"));
 const os_1 = __importDefault(require("os"));
 const helpers_1 = require("./helpers");
 const path_1 = __importDefault(require("path"));
 const chokidar_1 = __importDefault(require("chokidar"));
-var shell = os_1.default.platform() === 'win32' ? 'powershell.exe' : 'bash';
+var shell = os_1.default.platform() === "win32" ? "powershell.exe" : "bash";
 class RoomManager {
-    constructor() {
-        this.users = new Map();
-        this.watcher = chokidar_1.default.watch(path_1.default.join(__dirname, "..", "user"));
-        // watcher eventListners
-        this.watcher.on('add', (path) => {
-            console.log(`File ${path} is added`);
-            this.sendWatcherEvent('add', path);
+  constructor() {
+    this.users = new Map();
+    this.watcher = chokidar_1.default.watch(
+      path_1.default.join(__dirname, "..", "user")
+    );
+    // watcher eventListners
+    this.watcher
+      .on("add", (path) => {
+        console.log(`File ${path} is added`);
+        this.sendWatcherEvent("add", path);
+      })
+      .on("unlink", (path) => {
+        console.log(`File ${path} is removed`);
+        this.sendWatcherEvent("unlink", path);
+      })
+      .on("addDir", (path) => {
+        console.log(`Dir ${path} is added`);
+        this.sendWatcherEvent("addDir", path);
+      })
+      .on("unlinkDir", (path) => {
+        console.log(`Dir ${path} is removed`);
+        this.sendWatcherEvent("unlinkDir", path);
+      });
+  }
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new RoomManager();
+    }
+    return this.instance;
+  }
+  addToRoom(userId, socket) {
+    // spawn a new terminal
+    var ptyProcess = node_pty_1.spawn(shell, [], {
+      name: "xterm-color",
+      cols: 80,
+      rows: 30,
+      cwd: process.env.INIT_CWD + "/user",
+      env: process.env,
+    });
+    ptyProcess.onData((data) => {
+      socket.send(
+        JSON.stringify({
+          type: "terminal",
+          data: data,
         })
-            .on('unlink', (path) => {
-            console.log(`File ${path} is removed`);
-            this.sendWatcherEvent('unlink', path);
+      );
+      console.log(`${userId} sent: %s`, data);
+    });
+    ptyProcess.onExit((exitCode) => {
+      console.log(`${userId} exited with code %d`, exitCode);
+    });
+    this.users.set(userId, {
+      id: userId,
+      socket: socket,
+      pty: ptyProcess,
+    });
+    this.addListners(userId);
+  }
+  sendWatcherEvent(event, filePath) {
+    this.users.forEach((user) => {
+      user.socket.send(
+        JSON.stringify({
+          type: event,
+          data: filePath,
         })
-            .on('addDir', (path) => {
-            console.log(`Dir ${path} is added`);
-            this.sendWatcherEvent('addDir', path);
-        })
-            .on('unlinkDir', (path) => {
-            console.log(`Dir ${path} is removed`);
-            this.sendWatcherEvent('unlinkDir', path);
-        });
-    }
-    static getInstance() {
-        if (!this.instance) {
-            this.instance = new RoomManager();
-        }
-        return this.instance;
-    }
-    addToRoom(userId, socket) {
-        // spawn a new terminal 
-        var ptyProcess = node_pty_1.spawn(shell, [], {
-            name: 'xterm-color',
-            cols: 80,
-            rows: 30,
-            cwd: process.env.INIT_CWD + '/user',
-            env: process.env
-        });
-        ptyProcess.onData((data) => {
-            socket.send(JSON.stringify({
-                type: 'terminal',
-                data: data
-            }));
-            console.log(`${userId} sent: %s`, data);
-        });
-        ptyProcess.onExit((exitCode) => {
-            console.log(`${userId} exited with code %d`, exitCode);
-        });
-        this.users.set(userId, {
-            id: userId,
-            socket: socket,
-            pty: ptyProcess
-        });
-        this.addListners(userId);
-    }
-    sendWatcherEvent(event, filePath) {
-        this.users.forEach((user) => {
-            user.socket.send(JSON.stringify({
-                type: event,
-                data: filePath
-            }));
-        });
-    }
-    addListners(userId) {
-        var _a, _b;
-        const user = this.users.get(userId);
-        (_a = user.socket) === null || _a === void 0 ? void 0 : _a.on('message', (data) => __awaiter(this, void 0, void 0, function* () {
-            console.log('received: %s', data);
+      );
+    });
+  }
+  addListners(userId) {
+    var _a;
+    const user = this.users.get(userId);
+    (_a = user.socket) === null || _a === void 0
+      ? void 0
+      : _a.on("message", (data) =>
+          __awaiter(this, void 0, void 0, function* () {
+            console.log("received: %s", data);
             const payload = JSON.parse(data);
             switch (payload.type) {
-                case 'terminal': {
-                    user.pty.write(payload.data);
-                    console.log(user.pty.pid);
-                    break;
-                }
-                case 'filePatch': {
-                    this.users.forEach((u) => {
-                        if (u.id !== userId) {
-                            u.socket.send(JSON.stringify({
-                                type: 'filePatch',
-                                data: payload.data,
-                                filePath: payload.filePath
-                            }));
-                        }
-                    });
-                    yield (0, helpers_1.appyPatchtoFile)(path_1.default.join(__dirname, path_1.default.join('..', payload.filePath)), payload.data);
-                }
+              case "terminal": {
+                user.pty.write(payload.data);
+                console.log(user.pty.pid);
+                break;
+              }
+              case "filePatch": {
+                this.users.forEach((u) => {
+                  if (u.id !== userId) {
+                    u.socket.send(
+                      JSON.stringify({
+                        type: "filePatch",
+                        data: payload.data,
+                        filePath: payload.filePath,
+                      })
+                    );
+                  }
+                });
+                yield (0,
+                helpers_1.appyPatchtoFile)(path_1.default.join(__dirname, path_1.default.join("..", payload.filePath)), payload.data);
+              }
             }
-        }));
-        (_b = user.socket) === null || _b === void 0 ? void 0 : _b.on('close', () => {
-            this.users.delete(userId);
-            if (this.users.size === 0) {
-                process.exit(0);
-            }
-        });
-    }
+          })
+        );
+  }
 }
 exports.default = RoomManager;
--- backend/src/RoomManager.ts
+++ backend/src/RoomManager.ts
@@ -1,133 +1,127 @@
-import pty from 'node-pty';
-import os from 'os';
-import { WebSocket } from 'ws';
-import { appyPatchtoFile } from './helpers';
-import path from 'path';
-import chokidar, { FSWatcher } from 'chokidar';
+import pty from "node-pty";
+import os from "os";
+import { WebSocket } from "ws";
+import { appyPatchtoFile } from "./helpers";
+import path from "path";
+import chokidar, { FSWatcher } from "chokidar";
 
-
-
-interface User{
-    id:string,
-    socket:WebSocket,
-    pty:pty.IPty
+interface User {
+  id: string;
+  socket: WebSocket;
+  pty: pty.IPty;
 }
-var shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+var shell = os.platform() === "win32" ? "powershell.exe" : "bash";
 
+class RoomManager {
+  private users: Map<string, User> = new Map();
+  private static instance: RoomManager;
+  private watcher: FSWatcher;
 
-class RoomManager{
+  private constructor() {
+    this.watcher = chokidar.watch(path.join(__dirname, "..", "user"));
+    // watcher eventListners
+    this.watcher
+      .on("add", (path) => {
+        console.log(`File ${path} is added`);
+        this.sendWatcherEvent("add", path);
+      })
+      .on("unlink", (path) => {
+        console.log(`File ${path} is removed`);
+        this.sendWatcherEvent("unlink", path);
+      })
+      .on("addDir", (path) => {
+        console.log(`Dir ${path} is added`);
+        this.sendWatcherEvent("addDir", path);
+      })
+      .on("unlinkDir", (path) => {
+        console.log(`Dir ${path} is removed`);
+        this.sendWatcherEvent("unlinkDir", path);
+      });
+  }
 
-    private users: Map<string, User> = new Map();
-    private static instance: RoomManager;
-    private watcher: FSWatcher;
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new RoomManager();
+    }
+    return this.instance;
+  }
+
+  addToRoom(userId: string, socket: WebSocket) {
+    // spawn a new terminal
+    var ptyProcess = pty.spawn(shell, [], {
+      name: "xterm-color",
+      cols: 80,
+      rows: 30,
+      cwd: process.env.INIT_CWD + "/user",
+      env: process.env,
+    });
+    ptyProcess.onData((data) => {
+      socket.send(
+        JSON.stringify({
+          type: "terminal",
+          data: data,
+        })
+      );
+      console.log(`${userId} sent: %s`, data);
+    });
+    ptyProcess.onExit((exitCode) => {
+      console.log(`${userId} exited with code %d`, exitCode);
+    });
+
+    this.users.set(userId, {
+      id: userId,
+      socket: socket,
+      pty: ptyProcess,
+    });
+
+    this.addListners(userId);
+  }
+
+  private sendWatcherEvent(event: string, filePath: string) {
+    this.users.forEach((user) => {
+      user.socket.send(
+        JSON.stringify({
+          type: event,
+          data: filePath,
+        })
+      );
+    });
+  }
+
+  private addListners(userId: string) {
+    const user = this.users.get(userId)!;
+
+    user.socket?.on("message", async (data: string) => {
+      console.log("received: %s", data);
+      const payload = JSON.parse(data);
+      switch (payload.type) {
+        case "terminal": {
+          user.pty.write(payload.data);
+          console.log(user.pty.pid);
+          break;
+        }
+        case "filePatch": {
+          this.users.forEach((u) => {
+            if (u.id !== userId) {
+              u.socket.send(
+                JSON.stringify({
+                  type: "filePatch",
+                  data: payload.data,
+                  filePath: payload.filePath,
+                })
+              );
+            }
+          });
+          await appyPatchtoFile(
+            path.join(__dirname, path.join("..", payload.filePath)),
+            payload.data
+          );
+        }
+      }
+    });
 
     
-    private constructor(){
-        this.watcher = chokidar.watch(path.join(__dirname , ".." , "user"));
-         // watcher eventListners
-        this.watcher.on('add' , (path)=>{
-        console.log(`File ${path} is added`);
-        this.sendWatcherEvent('add' , path );
-        })
-        .on('unlink',(path)=>{
-        console.log(`File ${path} is removed`);
-        this.sendWatcherEvent('unlink' , path );
-        })
-        .on('addDir',(path)=>{
-        console.log(`Dir ${path} is added`);
-        this.sendWatcherEvent('addDir' , path);
-        })
-        .on('unlinkDir',(path)=>{
-        console.log(`Dir ${path} is removed`);
-        this.sendWatcherEvent('unlinkDir' , path );
-        })
-    }
-
-
-
-    static getInstance(){
-        if(!this.instance){
-            this.instance = new RoomManager();
-        }
-        return this.instance;
-    }
-
-    addToRoom(userId:string, socket:WebSocket){
-        // spawn a new terminal 
-        var ptyProcess = pty.spawn(shell, [], {
-            name: 'xterm-color',
-            cols: 80,
-            rows: 30,
-            cwd: process.env.INIT_CWD +'/user',
-            env: process.env
-        });
-        ptyProcess.onData((data) => {
-            socket.send(JSON.stringify({
-                type:'terminal',
-                data:data
-            }));
-            console.log(`${userId} sent: %s`, data);
-        });
-        ptyProcess.onExit((exitCode)=>{
-            console.log(`${userId} exited with code %d`, exitCode);
-        })
-
-        this.users.set(userId,{
-            id:userId,
-            socket:socket,
-            pty:ptyProcess
-        })
-
-        this.addListners(userId);
-    }
-
-    private sendWatcherEvent(event:string , filePath:string ){
-        this.users.forEach((user)=>{
-            user.socket.send(JSON.stringify({
-                type: event,
-                data:filePath
-            }))
-        })
-    }
-
-    private addListners(userId:string){
-        
-        const user = this.users.get(userId)!;
-
-        user.socket?.on('message',async(data:string)=>{
-            console.log('received: %s', data);
-            const payload = JSON.parse(data);
-            switch(payload.type){
-                case 'terminal':{
-                    user.pty.write(payload.data);
-                    console.log(user.pty.pid);
-                    break;
-                }
-                case 'filePatch':{
-                    this.users.forEach((u)=>{
-                        if(u.id !== userId){
-                            u.socket.send(JSON.stringify({
-                                type:'filePatch',
-                                data:payload.data,
-                                filePath:payload.filePath
-                            }))
-                        }
-                    })
-                    await appyPatchtoFile(path.join(__dirname,path.join('..' , payload.filePath)),payload.data);
-                    
-                }
-                    
-            }
-        })
-
-        user.socket?.on('close',()=>{
-            this.users.delete(userId);
-            if(this.users.size === 0){
-                process.exit(0);
-            }
-        })
-    }
+  }
 }
-
 
 export default RoomManager;
