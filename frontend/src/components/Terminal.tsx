94f32b347b6e3484c20301716f692b4abb157485
mohitejaikumar <mohitejaikumar@gmail.com>
2024-12-12 16:45:06 +0530

    resolved socket problem

--- frontend/src/components/Terminal.tsx
+++ frontend/src/components/Terminal.tsx
@@ -1,85 +1,78 @@
-import { useEffect, useRef} from "react";
-import {Terminal as XTerminal} from '@xterm/xterm';
-import { FitAddon } from '@xterm/addon-fit';
+import { useEffect, useRef } from "react";
+import { Terminal as XTerminal } from "@xterm/xterm";
+import { FitAddon } from "@xterm/addon-fit";
 import "@xterm/xterm/css/xterm.css";
 import useClient from "../hook/useClient";
 
+export default function Terminal() {
+  const terminalRef = useRef<HTMLDivElement>(null);
+  const rendered = useRef(false);
+  const { socket } = useClient();
+  const fitAddonRef = useRef<FitAddon | null>(null);
 
-export default function Terminal(){
+  console.log(socket);
 
-    const terminalRef = useRef<HTMLDivElement>(null);
-    const rendered = useRef(false);
-    const {socket} = useClient();
-    const fitAddonRef = useRef<FitAddon | null>(null);
-    
+  function handleResize() {
+    if (fitAddonRef.current === null || fitAddonRef.current === undefined) {
+      console.log("fitAddone is null");
+      return;
+    }
+    console.log("resize");
+    fitAddonRef.current.fit();
+  }
 
-    function handleResize(){
-        if(fitAddonRef.current === null || fitAddonRef.current === undefined){
-            console.log("fitAddone is null");
-            return;
-        }
-        console.log("resize");
-        fitAddonRef.current.fit();
+  useEffect(() => {
+    if (rendered.current === true || socket === null) {
+      return;
+    }
+    rendered.current = true;
+
+    const term = new XTerminal({
+      scrollSensitivity: 0,
+    });
+    const fitAddon = new FitAddon();
+    console.log("fitAddone", fitAddon);
+    fitAddonRef.current = fitAddon;
+    term.loadAddon(fitAddon);
+    term.open(terminalRef.current!);
+    fitAddon.fit();
+
+    term.onData((data) => {
+      socket.send(
+        JSON.stringify({
+          type: "terminal",
+          data: data,
+        })
+      );
+    });
+
+    socket.onmessage = (event) => {
+      const payload = JSON.parse(event.data);
+
+      switch (payload.type) {
+        case "terminal":
+          term.write(payload.data);
+          break;
+        default:
+          break;
+      }
+    };
+    const resizeObserver = new ResizeObserver(handleResize);
+
+    if (terminalRef.current) {
+      resizeObserver.observe(terminalRef.current);
     }
 
-    useEffect(()=>{
+    return () => {
+      if (terminalRef.current) {
+        resizeObserver.unobserve(terminalRef.current);
+      }
+    };
+  }, []);
 
-        if(rendered.current === true || socket === null){
-            return;
-        }
-        rendered.current = true;
-        
-
-        const term = new XTerminal({
-            scrollSensitivity:0
-        });
-        const fitAddon = new FitAddon();
-        console.log("fitAddone" , fitAddon);
-        fitAddonRef.current = fitAddon;
-        term.loadAddon(fitAddon);
-        term.open(terminalRef.current!);
-        fitAddon.fit();
-
-        
-        term.onData((data)=>{
-            
-            socket.send(JSON.stringify({
-                type:'terminal',
-                data:data
-            }));
-        });
-
-        socket.onmessage = (event)=>{
-            const payload = JSON.parse(event.data);
-
-            switch(payload.type){
-                case 'terminal':
-                    term.write(payload.data);
-                    break;
-                default:
-                    break;
-            }
-            
-        }
-        const resizeObserver = new ResizeObserver(handleResize);
-
-        if (terminalRef.current) {
-        resizeObserver.observe(terminalRef.current);
-        }
-
-        return () => {
-            if (terminalRef.current) {
-                resizeObserver.unobserve(terminalRef.current);
-            }
-        };
-        
-    },[socket])
-
-    return (
-        <div 
-            className="overflow-x-hidden overflow-y-hidden h-full bg-black" 
-            ref={terminalRef}
-        >
-        </div>
-    )
+  return (
+    <div
+      className="overflow-x-hidden overflow-y-hidden h-full bg-black"
+      ref={terminalRef}></div>
+  );
 }
